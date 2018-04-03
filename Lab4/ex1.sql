8ec00bb38428a4f1c8da834f9ce20b7df6af2bd3
Dan <danpopa1998@gmail.com>
2018-04-04 00:29:41 +0300

    finish ex1

--- Lab4/ex1.sql
+++ Lab4/ex1.sql
@@ -1,7 +1,18 @@
+IF NOT firme DROP TABLE firme;
+IF NOT agenti DROP TABLE agenti;
+
 CREATE TABLE firme (
 	codfirma SMALLINT PRIMARY KEY,
 	denfirma VARCHAR(20) NOT NULL,
 	loc VARCHAR(20),
-	zona VARCHAR(15) CONSTRAINT FZONA_CH check (zona IN
+	zona VARCHAR(15) check (zona IN
 	('Moldova','Ardeal','Banat','Muntenia','Dobrogea','Transilvania')) 
-)
+);
+
+CREATE TABLE agenti (
+	codagent VARCHAR(3) PRIMARY KEY
+	numeagent VARCHAR(25) NOT NULL
+	dataang DATE 	DEFAULT SYSDATE	
+	functia	VARCHAR(20)	
+	codfirma	NUMBER(2) NSTRAINT FK_agenti FOREIGN KEY (codfirma) REFERENCES firme(codfirma)
+);
