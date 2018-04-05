34799e55632303d4c09dd1d71d0bd789d7da4771
Dan <danpopa1998@gmail.com>
2018-04-05 13:48:50 +0300

    continue lab 5

--- Lab5/lab5_1.sql
+++ Lab5/lab5_1.sql
@@ -1,4 +1,15 @@
-select * from ANGAJATI;
+--	select * from ANGAJATI;
+--	select NUME, PRENUME from ANGAJATI;
+--	select NUME, PRENUME, SALARIUL from ANGAJATI;
+--	select NUME, PRENUME, SALARIUL as sal from ANGAJATI;
 
-select NUME, PRENUME from ANGAJATI;
-select NUME, PRENUME, SALARIUL from ANGAJATI;
+-- select * from ANGAJATI where SALARIUL > 10000;
+
+-- SELECT * FROM angajati where nume like 'M%' and SALARIUL < 10000;
+
+-- SELECT * FROM angajati where ID_DEPARTAMENT = 100;
+
+-- SELECT id_angajat, id_functie, data_angajare as 'lucreaza din' from angajati;
+
+SELECT  id_functie FROM angajati;
+SELECT DISTINCT id_functie FROM angajati;
