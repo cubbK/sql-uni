467c370763db78f8dcd72afb70c4fca1867218e2
Dan <danpopa1998@gmail.com>
2018-04-05 14:30:18 +0300

    more lab 5

--- Lab5/lab5_1.sql
+++ Lab5/lab5_1.sql
@@ -11,5 +11,12 @@
 
 -- SELECT id_angajat, id_functie, data_angajare as 'lucreaza din' from angajati;
 
-SELECT  id_functie FROM angajati;
-SELECT DISTINCT id_functie FROM angajati;
+-- SELECT  id_functie FROM angajati;
+-- SELECT DISTINCT id_functie FROM angajati;
+
+-- SELECT * from angajati;
+
+-- SELECT nume, data_angajare, id_functie FROM angajati WHERE nume = 'Taylor' OR  nume  = 'Matos'  ORDER BY data_angajare;
+
+
+-- SELECT nume, data_angajare, id_functie FROM angajati WHERE nume IN ('Mantos', 'Taylor')  ORDER BY data_angajare;
