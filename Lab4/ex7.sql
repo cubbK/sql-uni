2549e2be1c8a994282a7b7d24a7c5665c4ea6f3d
Dan <danpopa1998@gmail.com>
2018-04-18 21:01:44 +0300

    add ex7

---
+++ Lab4/ex7.sql
@@ -0,0 +1,8 @@
+CREATE TABLE salariati LIKE angajati;
+
+INSERT INTO salariati
+VALUES (152, 'Andrei', 'manager', 852);
+
+INSERT INTO salariati
+SELECT * FROM angajati
+WHERE departament IN (20, 30, 50);
