fce28575d1b1ff507e5f337b614664eb5d710e4a
Dan <danpopa1998@gmail.com>
2018-04-18 22:29:44 +0300

    add ex 8 and 9

---
+++ Lab4/ex8.sql
@@ -0,0 +1,4 @@
+UPDATE salariati
+SET column1 = value1, column2 = value2
+FROM 
+WHERE salariul < 3000;
---
+++ Lab4/ex9.sql
@@ -0,0 +1,5 @@
+DELETE FROM salariati WHERE id_manager=122;
+
+DELETE FROM salariati WHERE YEAR(date) < 2016;
+
+DELETE FROM salariati;
