8d3c2af3134055bc3210f01a399801f2ff43a132
Dan <danpopa1998@gmail.com>
2018-04-24 02:36:10 +0300

    write in lab5

--- Lab5/lab5_1.sql
+++ Lab5/lab5_1.sql
@@ -1,22 +1,23 @@
---	select * from ANGAJATI;
---	select NUME, PRENUME from ANGAJATI;
---	select NUME, PRENUME, SALARIUL from ANGAJATI;
---	select NUME, PRENUME, SALARIUL as sal from ANGAJATI;
+	select * from ANGAJATI;
+	select NUME, PRENUME from ANGAJATI;
+	select NUME, PRENUME, SALARIUL from ANGAJATI;
+	select NUME, PRENUME, SALARIUL as sal from ANGAJATI;
 
--- select * from ANGAJATI where SALARIUL > 10000;
+select * from ANGAJATI where SALARIUL > 10000;
 
--- SELECT * FROM angajati where nume like 'M%' and SALARIUL < 10000;
+SELECT * FROM angajati where nume like 'M%' and SALARIUL < 10000;
 
--- SELECT * FROM angajati where ID_DEPARTAMENT = 100;
+SELECT * FROM angajati where ID_DEPARTAMENT = 100;
 
--- SELECT id_angajat, id_functie, data_angajare as 'lucreaza din' from angajati;
+SELECT id_angajat, id_functie, data_angajare as 'lucreaza din' from angajati;
 
--- SELECT  id_functie FROM angajati;
--- SELECT DISTINCT id_functie FROM angajati;
+SELECT  id_functie FROM angajati;
+SELECT DISTINCT id_functie FROM angajati;
 
--- SELECT * from angajati;
+SELECT * from angajati;
 
--- SELECT nume, data_angajare, id_functie FROM angajati WHERE nume = 'Taylor' OR  nume  = 'Matos'  ORDER BY data_angajare;
+SELECT nume, data_angajare, id_functie FROM angajati WHERE nume = 'Taylor' OR  nume  = 'Matos'  ORDER BY data_angajare;
 
 
--- SELECT nume, data_angajare, id_functie FROM angajati WHERE nume IN ('Mantos', 'Taylor')  ORDER BY data_angajare;
+SELECT nume, data_angajare, id_functie FROM angajati WHERE nume IN ('Mantos', 'Taylor')  ORDER BY data_angajare;
+
